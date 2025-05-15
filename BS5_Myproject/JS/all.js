8b2df007f2d5bcb34628f55b3a536a97d972178a
Xenosword-X <swordsgod790626@gmail.com>
2025-05-15 17:24:50 +0800

    5/15更新

--- BS5_Myproject/JS/all.js
+++ BS5_Myproject/JS/all.js
@@ -9,37 +9,5 @@ $(document).ready(function () {
             $(this).find('img').attr('src', './images/robot01.jpg');
             $(this).find('p').removeClass('bg-primary text-white').addClass('bg-white');
         }
-    );
-
-    //首頁二手良品卡片hover樣式
-    //$('.hover-card2').hover(
-    //    function(){
-    //        $(this).find('img').attr('src','./images/PSVR02.png')
-    //        $(this).find('.btn').css('color','#0d6efd')
-    //    },
-    //    function(){
-    //        $(this).find('img').attr('src','./images/PSVR01.jpg')
-    //        $(this).find('.btn').css('color','#212529')
-    //    }
-    //);
-    $('.hover-card3').hover(
-        function(){
-            $(this).find('img').attr('src','./images/PS3-02.png')
-            $(this).find('.btn').css('color','#0d6efd')
-        },
-        function(){
-            $(this).find('img').attr('src','./images/PS3-01.png')
-            $(this).find('.btn').css('color','#212529')
-        }
-    );
-    $('.hover-card4').hover(
-        function(){
-            $(this).find('img').attr('src','./images/PSC-02.jpg')
-            $(this).find('.btn').css('color','#0d6efd')
-        },
-        function(){
-            $(this).find('img').attr('src','./images/PSC-01.jpg')
-            $(this).find('.btn').css('color','#212529')
-        }
-    );
+    );    
 });
